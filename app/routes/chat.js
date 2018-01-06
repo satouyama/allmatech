d8e3922ffec9e8d3e53025073e8c185f76509dbb
Douglas <senhorsatou@gmail.com>
2018-01-06 17:38:30 -0200

    [FIX]

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,28 +1,28 @@
 module.exports= function(app){
 app.get('/chat', function(req ,res){
-    var dadosForm = req.body;
-    console.log(dadosForm);
-  	app.get('io').emit(
-  		'msgParaCliente',
-
-  		{apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
-
-  	)
-
-  	res.render("chat", {dadosForm : dadosForm});
+	var dadosForm = req.body;
+	
+		
+		
+	
+		app.get('io').emit(
+			'msgParaCliente',
+			{apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
+		)
+	
+		res.render("chat", {dadosForm : dadosForm});
 
 });
 app.post('/chat', function(req ,res){
-    let dadosForm = req.body;
-    console.log(dadosForm);
-  	app.get('io').emit(
-  		'msgParaCliente',
-
-  		{apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
-
-  	)
-
-  	res.render("chat", {dadosForm : dadosForm});
+	var dadosForm = req.body;
+	
+	
+		app.get('io').emit(
+			'msgParaCliente',
+			{apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
+		)
+	
+		res.render("chat", {dadosForm : dadosForm});
 
 });
 }
